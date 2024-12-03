6d1316de85ba5a53c572cbe9edcc824119000f8f
geetix-prog <mathis.guellati@edu.univ-fcomte.fr>
2024-12-03 20:31:40 +0100

    const js

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,18 @@
 // *** CAROUSEL ***
+const carousel = document.querySelector('.carousel__container');
+const prevButton = document.querySelector('.carousel__button--prev');
+const nextButton = document.querySelector('.carousel__button--next');
 
+// Largeur de défilement d’un item
+if (carousel) {
+  // Scroll au clic sur le bouton précédent
+  prevButton.____________("_____", () => {
+    ______________________;
+  });
+
+  // Scroll au clic sur le bouton suivant
+  nextButton.____________("_____", () => {
+    ______________________;
+  });
+}
 // TODO
